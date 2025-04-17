9fe02aa37edead2629488f294ef7334e1df95332
Amr Abdel Hamide <amrabdelhamideeshiba@gmail.com>
2025-04-17 20:38:41 +0200

    edit#3

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,7 +21,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://elkhateb-22.github.io",
+  site: "https://bad-glitch.github.io",
   base: "blog",
   trailingSlash: "always",
   integrations: [
